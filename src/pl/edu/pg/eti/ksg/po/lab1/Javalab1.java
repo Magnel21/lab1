1ce4baf7a1f1c1a00366d59b6cfe4c5d83efa77b
Magnel <aleksander,makowski@op.pl>
2025-03-14 18:17:12 +0100

    init

---
+++ src/pl/edu/pg/eti/ksg/po/lab1/Javalab1.java
@@ -0,0 +1,10 @@
+package pl.edu.pg.eti.ksg.po.lab1;
+import java.util.Scanner;
+
+public class Javalab1 {
+    public static void main(String[] args)
+    {
+        A obj = new A(4);
+        System.out.println(obj.hashCode());
+    }
+}
